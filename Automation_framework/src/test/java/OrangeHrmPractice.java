e0680ef4c0528dbf20c8169013c30f221bfa91a1
Richard Andronache <randronache@deloittece.com>
2023-08-17 21:14:43 +0300

    Return a text from an URL

--- Automation_framework/src/test/java/OrangeHrmPractice.java
+++ Automation_framework/src/test/java/OrangeHrmPractice.java
@@ -2,9 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.*;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class OrangeHrmPractice {
     public static void main(String[] args) {
@@ -13,8 +14,14 @@ public class OrangeHrmPractice {
         String title = driver.getTitle();
 
         String URL = driver.getCurrentUrl();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        String text = driver.findElement(By.cssSelector(".oxd-text.oxd-text--h5.orangehrm-login-title")).getText();
+
         System.out.println(title);
         System.out.println(URL);
+        System.out.println(text);
+
         driver.close();
     }
 
