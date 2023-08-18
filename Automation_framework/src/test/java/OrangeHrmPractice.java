7747c1c3250fe14ed7e6660545359ccd2e19b4db
Richard Andronache <randronache@deloittece.com>
2023-08-18 17:19:30 +0300

    return row, columns number and a value from a table

--- Automation_framework/src/test/java/OrangeHrmPractice.java
+++ Automation_framework/src/test/java/OrangeHrmPractice.java
@@ -13,22 +13,22 @@ public class OrangeHrmPractice {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        String title = driver.getTitle();// show title of the page
+        String title = driver.getTitle();
 
-        String URL = driver.getCurrentUrl();//show url
+        String URL = driver.getCurrentUrl();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        String text = driver.findElement(By.cssSelector(".oxd-text.oxd-text--h5.orangehrm-login-title")).getText();//show the text ina web page for button login
+        String text = driver.findElement(By.cssSelector(".oxd-text.oxd-text--h5.orangehrm-login-title")).getText();
 
-        String attribute = driver.findElement(By.xpath("//form[@class = \"oxd-form\"]")).getAttribute("method"); //take the attribute from value class
+        String attribute = driver.findElement(By.xpath("//form[@class = \"oxd-form\"]")).getAttribute("method");
 
-        Boolean textBox = driver.findElement(By.xpath("//p[@class=\"oxd-text oxd-text--p\"][2]")).isEnabled();//return true or false if the element exists
+        Boolean textBox = driver.findElement(By.xpath("//p[@class=\"oxd-text oxd-text--p\"][2]")).isEnabled();
 
-        System.out.println(title);
-        System.out.println(URL);
-        System.out.println(text);
-        System.out.println(attribute);
-        System.out.println(textBox);
+        System.out.println(title);// show title of the page
+        System.out.println(URL);//show url
+        System.out.println(text);//show the text ina web page for button login
+        System.out.println(attribute);//take the attribute from value class
+        System.out.println(textBox);//return true or false if the element exists
 
         driver.close();
     }
---
+++ Automation_framework/src/test/java/Tables.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class Tables {
+
+    //Test to handle dynamic web table using Selenium WebDriver
+
+    WebDriver driver = new ChromeDriver();
+
+        @Test
+        public void tripDetails() throws InterruptedException, AWTException
+        {
+//Modify Wait time as per the Network Ability in the Thread Sleep method
+
+            driver.get("https://cosmocode.io/automation-practice-webtable/");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            Thread.sleep(5000);
+
+//Finding number of Rows
+
+            List<WebElement> rowsNumber = driver.findElements(By.xpath("//table[@id=\"countries\"]/tbody/tr"));
+            int rowCount = rowsNumber.size();
+            System.out.println("No of rows in this table : " + rowCount);
+
+//Finding number of Columns
+
+            List<WebElement> columnsNumber = driver.findElements(By.xpath("//table[@id=\"countries\"]/tbody/tr/td/h3"));
+            int columnCount = columnsNumber.size();
+            System.out.println("No of columns in this table : " + columnCount);
+
+//Finding cell value at 4th row and 3rd column
+
+            WebElement cellAddress = driver.findElement(By.xpath("//table[@id=\"countries\"]/tbody/tr[4]/td[2]"));
+            String value = cellAddress.getText();
+            System.out.println("The Cell Value is : " +value);
+
+            driver.quit();
+        }
+
+    }
+
